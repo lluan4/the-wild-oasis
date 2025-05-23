5b8382a20ed3d8c0be1405684e8655dfb713a235
Luan Lima <lluan.lima94@gmail.com>
2025-05-22 22:26:28 -0300

    fix: remove console.log statements from supabase service

--- src/shared/services/supabase.ts
+++ src/shared/services/supabase.ts
@@ -3,9 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY as string;
 export const SUPA_BASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
 
-console.log(import.meta.env.VITE_SUPABASE_KEY);
-console.log(import.meta.env.VITE_SUPABASE_URL);
-
 const supabase = createClient(SUPA_BASE_URL, SUPABASE_KEY);
 
 export default supabase;
